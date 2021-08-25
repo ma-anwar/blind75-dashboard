6bf926a34a3cd3aa29e6b1619a977791f43ec907
ma-anwar <ma.rizvi.anwar@gmail.com>
2021-08-24 23:22:48 -0400

    Setup

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import "@fontsource/roboto";
+import { Paper, Button } from "@material-ui/core/";
+
+function App() {
+  return (
+    <div className="App">
+      <Paper>
+        <p>Hello World</p>
+        <Button variant="contained">Log in</Button>
+      </Paper>
+    </div>
+  );
+}
+
+export default App;
