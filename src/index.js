ee77301d283fa62efcde55660ce7c9dab87b07f4
ma-anwar <ma.rizvi.anwar@gmail.com>
2021-08-25 14:45:29 -0400

    Implement auth and rough components

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
 import App from "./Components/App/App";
 import theme from "./theme";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    <CssBaseline />
-    <App />
-  </ThemeProvider>,
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+    redirectUri={window.location.origin}>
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </Auth0Provider>,
   document.getElementById("root")
 );
