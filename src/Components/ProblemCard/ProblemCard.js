c73e3f384b9a0ba7c67002cae71ac8bd01662da2
ma-anwar <ma.rizvi.anwar@gmail.com>
2021-08-26 03:05:51 -0400

    Finish implementing masonry layout of problems (#3)

--- src/Components/ProblemCard/ProblemCard.js
+++ src/Components/ProblemCard/ProblemCard.js
@@ -1,15 +1,54 @@
-import { Card, CardContent, Typography, Box } from "@material-ui/core";
+import { Checkbox, FormControlLabel } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Link from "@material-ui/core/Link";
+import { Column, Row, Item } from "@mui-treasury/components/flex";
+
+const useStyles = makeStyles(() => ({
+  card: {
+    width: "100%",
+    borderRadius: 16,
+    boxShadow: "0 8px 16px 0 #BDC9D7",
+    overflow: "hidden",
+  },
+  header: {
+    fontFamily: "Barlow, san-serif",
+    backgroundColor: "#fff",
+  },
+  headline: {
+    color: "#122740",
+    fontSize: "1.25rem",
+    fontWeight: 600,
+  },
+  link: {
+    color: "#2281bb",
+    padding: "0 0.25rem",
+    fontSize: "0.875rem",
+  },
+  actions: {
+    color: "#BDC9D7",
+  },
+  divider: {
+    backgroundColor: "#d9e2ee",
+    margin: "0 20px",
+  },
+}));
 
 export default function ProblemCard({ title, link }) {
+  const styles = useStyles();
   return (
-    <Card>
-      <CardContent>
-        <Box component="span" display="inline">
-          <Typography>
-            <a href={link}> {title} </a>{" "}
-          </Typography>
-        </Box>
-      </CardContent>
-    </Card>
+    <Column p={0} gap={0} className={styles.card}>
+      <Row wrap p={2} alignItems={"baseline"} className={styles.header}>
+        <Item stretched className={styles.headline}>
+          <Link className={styles.link} href={link}>
+            {title}
+          </Link>
+        </Item>
+        <Item className={styles.actions}>
+          <FormControlLabel
+            control={<Checkbox name="checkedB" color="primary" />}
+          />
+        </Item>
+      </Row>
+    </Column>
   );
 }
--- src/Components/TitleCard/TitleCard.js
+++ src/Components/TitleCard/TitleCard.js
@@ -1,1 +1,67 @@
-export default function TitleCard({ title }) {}
+import { Column, Row, Item } from "@mui-treasury/components/flex";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
+import Link from "@material-ui/core/Link";
+import ProblemCard from "../ProblemCard/ProblemCard";
+import { problems } from "../App/data";
+
+const useStyles = makeStyles(() => ({
+  card: {
+    borderRadius: 16,
+    boxShadow: "0 8px 16px 0 #BDC9D7",
+    overflow: "hidden",
+  },
+  header: {
+    fontFamily: "Barlow, san-serif",
+    backgroundColor: "#fff",
+  },
+  headline: {
+    color: "#122740",
+    fontSize: "1.25rem",
+    fontWeight: 600,
+  },
+  link: {
+    color: "#2281bb",
+    padding: "0 0.25rem",
+    fontSize: "0.875rem",
+  },
+  actions: {
+    color: "#BDC9D7",
+  },
+  divider: {
+    backgroundColor: "#d9e2ee",
+    margin: "0 20px",
+  },
+}));
+
+export default function TitleCard({ title }) {
+  function generateProblems() {
+    const items = problems.reduce((filtered, item) => {
+      if (item.category === title) {
+        filtered.push(item);
+      }
+      return filtered;
+    }, []);
+    return items;
+  }
+  const styles = useStyles();
+  return (
+    <Column p={0} gap={0} className={styles.card}>
+      <Row wrap p={2} alignItems={"baseline"} className={styles.header}>
+        <Item stretched className={styles.headline}>
+          {title}
+        </Item>
+        <Item className={styles.actions}>
+          <Link className={styles.link}>Hide</Link>
+        </Item>
+      </Row>
+      {generateProblems().map((problem, i) => (
+        <div>
+          <ProblemCard link={problem.link} title={problem.title} />
+          <Divider variant={"middle"} className={styles.divider} />
+        </div>
+      ))}
+    </Column>
+  );
+}
