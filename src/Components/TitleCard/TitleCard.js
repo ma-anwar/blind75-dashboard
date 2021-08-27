2b5dac05667ec15c4f36f2894a0909bcf136d92c
ma-anwar <ma.rizvi.anwar@gmail.com>
2021-08-27 01:02:43 -0400

    Restyle problem and titlecards (#9)

--- src/Components/TitleCard/TitleCard.js
+++ src/Components/TitleCard/TitleCard.js
@@ -1,26 +1,22 @@
-import { Column, Row, Item } from "@mui-treasury/components/flex";
 import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
-import Divider from "@material-ui/core/Divider";
-import Link from "@material-ui/core/Link";
+import { Box, Link, Divider, Typography } from "@material-ui/core";
 import ProblemCard from "../ProblemCard/ProblemCard";
 import { problems } from "../App/data";
 
 const useStyles = makeStyles(() => ({
   card: {
-    borderRadius: 16,
-    boxShadow: "0 8px 16px 0 #BDC9D7",
+    borderRadius: 12,
+    boxShadow: "0 4px 10px 0 #BDC9D7",
     overflow: "hidden",
   },
   header: {
     fontFamily: "Barlow, san-serif",
     backgroundColor: "#fff",
-  },
-  headline: {
     color: "#122740",
     fontSize: "1.25rem",
     fontWeight: 600,
   },
+  headline: {},
   link: {
     color: "#2281bb",
     padding: "0 0.25rem",
@@ -50,21 +46,19 @@ export default function TitleCard({ title }) {
   }
   const styles = useStyles();
   return (
-    <Column p={0} gap={0} className={styles.card} m={1}>
-      <Row wrap p={2} alignItems={"baseline"} className={styles.header}>
-        <Item stretched className={styles.headline}>
-          {getTitle(title)}
-        </Item>
-        <Item className={styles.actions}>
-          <Link className={styles.link}>Hide</Link>
-        </Item>
-      </Row>
+    <Box m={2} className={styles.card}>
+      <Box display="flex" mx={1} mt={1} justifyContent="space-between">
+        <Typography variant="h6" noWrap className={styles.header}>
+          {getTitle(title)}{" "}
+        </Typography>
+        <Link className={styles.link}>Hide</Link>
+      </Box>
       {getProblems().map((problem, i) => (
         <div>
           <ProblemCard link={problem.link} title={problem.title} />
           <Divider variant={"middle"} className={styles.divider} />
         </div>
       ))}
-    </Column>
+    </Box>
   );
 }
