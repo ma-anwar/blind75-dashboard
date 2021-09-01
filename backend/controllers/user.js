1598ab5b032de18621496652ec2961b5f2411581
ma-anwar <ma.rizvi.anwar@gmail.com>
2021-09-01 18:54:50 -0400

    Refactor directory structure for backend Implement resolvers

---
+++ backend/controllers/user.js
@@ -0,0 +1,64 @@
+const User = require("../models/userModel");
+
+// @desc   Get all problems
+// @route  GET /api/v1/problem
+exports.getProblems = async (req, res, next) => {
+  //sub is part of the decoded jwt and represents a uid, used as primary key for now
+
+  const key = { uid: req.user.sub };
+  const selector = "problems";
+  try {
+    const user = await User.find(key, selector).exec();
+    const problems = user[0].problems.map((problem) => problem.title);
+    return res.status(200).json({
+      success: true,
+      problems: problems,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      error: "Internal Error",
+    });
+  }
+};
+
+// @desc   Add to problems
+// @route  POST /api/v1/problem
+exports.addProblem = async (req, res, next) => {
+  const key = { uid: req.user.sub };
+  const data = { $push: { problems: { title: req.body.title } } };
+  try {
+    const user = await User.updateOne(key, data, { upsert: "true" });
+    return res.status(201).json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      error: "Internal Error",
+    });
+  }
+};
+
+// @desc   Delete Problem
+// @route  DELETE /api/v1/problem
+exports.deleteProblem = async (req, res, next) => {
+  const key = { uid: req.user.sub };
+  const data = { $pull: { problems: { title: req.body.title } } };
+  try {
+    const user = await User.updateOne(key, data, { upsert: "true" });
+    return res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      error: "Internal Error",
+    });
+  }
+};
---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+const { checkJwt } = require("./config/auth");
+const connectDB = require("./config/db");
+
+dotenv.config({ path: "./config/config.env" });
+
+(async () => {
+  await connectDB();
+
+  const user = require("./routes/user");
+
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+
+  if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+  }
+
+  app.use(checkJwt);
+  app.use("/api/v1/user", user);
+
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server started on port ${PORT} 🚀🚀🚀`));
+})();
