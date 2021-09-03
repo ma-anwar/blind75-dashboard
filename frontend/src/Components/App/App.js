e115781f4d63fd88e55529d81a651f7708b6386f
ma-anwar <ma.rizvi.anwar@gmail.com>
2021-09-03 02:07:17 -0400

    Implement api calls on frontend

--- frontend/src/Components/App/App.js
+++ frontend/src/Components/App/App.js
@@ -1,21 +1,64 @@
-import { Paper, Typography, Box } from "@material-ui/core/";
+import { Paper, Typography } from "@material-ui/core/";
 import LoginButton from "../LoginButton/LoginButton";
 import TitleCard from "../TitleCard/TitleCard";
 import { categories } from "./data";
 import Masonry from "react-masonry-css";
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from "react";
 import { useStyles } from "./AppStyles.js";
+import { problems } from "../App/data";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const [data, setData] = useState([]);
+
   useEffect(() => {
-    document.title = "Blind-75"
-  }, [])
+    async function getCompletedProblems() {
+      try {
+        const accessToken = await getAccessTokenSilently({
+          audience: `https://b75db`,
+        });
+        const endpoint = `http://localhost:4001/api/v1/user/`;
+
+        const response = await fetch(endpoint, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+          method: "GET",
+        });
+
+        const responseBody = await response.json();
+        return responseBody;
+      } catch (error) {
+        console.log(error.message);
+        return null;
+      }
+    }
+
+    (async function generateProblemSet() {
+      console.log("Running");
+      const completedProblems = await getCompletedProblems();
+      if (!completedProblems) {
+        setData(problems.map((item) => ({ ...item, completed: false })));
+        console.log("failed");
+        return;
+      }
+
+      const problemSet = await problems.map((item) => ({
+        ...item,
+        completed: completedProblems.problems.includes(item.title),
+      }));
+      setData(problemSet);
+    })();
+  }, [getAccessTokenSilently]);
+
   const styles = useStyles();
   const breakpointObject = {
     default: 3,
     1000: 2,
     700: 1,
   };
+
   return (
     <div className="App">
       <Paper>
@@ -28,7 +71,7 @@ function App() {
           className={styles.masonryGrid}
           columnClassName={styles.masonryColumn}>
           {categories.map((category, i) => (
-            <TitleCard title={category} />
+            <TitleCard title={category} problems={data} />
           ))}
         </Masonry>
       </Paper>
